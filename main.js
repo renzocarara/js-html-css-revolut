ee76715bf8a2909554b2671f2c7ad18a617e2700
Renzo Carara <renzo.carara@libero.it>
2019-11-26 23:34:53 +0100

    gestito caso per dismettere un dropdown menu cliccando in un punto qualsiasii della pagina, implementata classe .active per voci menu (mette il testo in nero)

--- main.js
+++ main.js
@@ -1,32 +1,66 @@
 $(document).ready(function() {
 
-    console.log("doc is ready");
-
     // NON FUNZIONA mouseenter, PERCHE'????
-    //catturo il mouseenter su menu Accounts
-    // $('#accounts').mouseover(function() {
+    //catturo il mouseenter sulle voci del menu
+    // $('.menu-item').mouseenter(function() {
     //     console.log("mouseenter catturato!");
     //     // apro o chiudo il dropdown menu
-    //     $('#accountsDDmenu').slideToggle();
+    //     $('this').children(dd-menu).slideToggle();
     // });
 
     // GRAZIE A LUCA!  PER LE MEGA-DRITTE...
-    // intercetto il click sulla voce menu
+    // intercetto il click sulla voce menu attraveso la sua classe
     $(".menu-item").click(function() {
-        console.log("menu-item cliccato");
 
-        // verifico che il menu cliccato non sia già aperto
-        // se fosse così non faccio nulla, lo lascio visualizzato
+        // verifico che il menu cliccato non sia già visualizzato ma sia
+        // nascosto (hidden) quindi da visualizzare
+        // se non fosse così non faccio nulla, lo lascio visualizzato
         // alrimenti entro nell'if, chiudo eventuali altri menu aperti
         // e poi visualizzo quello che è stato cliccato
-        if ($(this).children(".DDmenu").is(":hidden")) {
+        if ($(this).children(".dd-menu").is(":hidden")) {
 
             // chiudo tutti i dropdown menu eventualmente aperti
-            $(".menu-item").children(".DDmenu").slideUp();
+            $(".menu-item").children(".dd-menu").slideUp();
+            // rimuovo l'evidenziazione "attiva" dalla voce del menu
+            $(".menu-item").removeClass("active-menu");
             // apro il dropdown menu della voce menu cliccata
-            $(this).children(".DDmenu").slideToggle();
-
+            $(this).children(".dd-menu").slideToggle();
+            // evidenzio la voce del menu come "attiva"
+            $(this).addClass("active-menu");
         }
-    })
+    });
 
-}) // document ready
+    // se c'è un dropdown menu aperto, e l'utente clicca in qualunque
+    // punto sul documento (eccetto un menu dropdown), nascondo il dropdown menu
+    $(document).click(function(event) {
+        // se l'elemento cliccto non è una voce dei menu dropdown
+        // allora chiudo gli eventuali dropdown menu aperti
+        if (!$(event.target).closest('.menu-item').length) {
+            // chiudo tutti i dropdown menu eventualmente aperti
+            $(".menu-item").children(".dd-menu").slideUp();
+            // rimuovo l'evidenziazione "attiva" dalla voce del menu
+            $(".menu-item").removeClass("active-menu");
+        }
+    });
+
+
+    // intercetto click sulla dropdown menu per il cambio lingua
+    $('#menu-right-list li:nth-child(6) .dd-menu span').click(function() {
+
+        // console.log("$(this).text", $(this).text());
+
+        switch ($(this).text()) {
+            case 'Italiano':
+                // console.log("it");
+                $('#language-menu').text("IT");
+                break;
+            case 'English':
+                // console.log("eng");
+                $('#language-menu').text("EN");
+                break;
+            default:
+        }
+
+    });
+
+}); // document ready
