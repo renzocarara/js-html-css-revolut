0249e5ab363ab7808f77c2c8de9969d7d910d0cc
Renzo Carara <renzo.carara@libero.it>
2019-11-26 18:13:16 +0100

    risolto utilizzando selettore jquery :hidden per verificare se il menu dropdown cliccato è già visibile, nel qual caso non faccio nulla

--- main.js
+++ main.js
@@ -13,14 +13,20 @@ $(document).ready(function() {
     // GRAZIE A LUCA!  PER LE MEGA-DRITTE...
     // intercetto il click sulla voce menu
     $(".menu-item").click(function() {
-        // console.log("menu-item cliccato");
+        console.log("menu-item cliccato");
 
-        // chiudo tutti i dropdown menu eventualmente aperti
-        $(".menu-item").children(".DDmenu").slideUp();
-        // apro il dropdown menu della voce menu cliccata
-        $(this).children(".DDmenu").slideToggle();
+        // verifico che il menu cliccato non sia già aperto
+        // se fosse così non faccio nulla, lo lascio visualizzato
+        // alrimenti entro nell'if, chiudo eventuali altri menu aperti
+        // e poi visualizzo quello che è stato cliccato
+        if ($(this).children(".DDmenu").is(":hidden")) {
 
+            // chiudo tutti i dropdown menu eventualmente aperti
+            $(".menu-item").children(".DDmenu").slideUp();
+            // apro il dropdown menu della voce menu cliccata
+            $(this).children(".DDmenu").slideToggle();
 
+        }
     })
 
 }) // document ready
