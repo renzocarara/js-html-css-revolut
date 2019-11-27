a24803bb7c3fcd18eaf3a3bdee9f4e40385fdfe4
Renzo Carara <renzo.carara@libero.it>
2019-11-27 09:16:03 +0100

    sistemati commenti

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ $(document).ready(function() {
         // verifico che il menu selezionato non sia già visualizzato ma sia
         // nascosto (hidden) quindi da visualizzare
         // se non fosse così non faccio nulla, lo lascio visualizzato
-        // alrimenti entro nell'if, chiudo eventuali altri menu aperti
+        // altrimenti entro nell'if, chiudo eventuali altri menu aperti
         // e poi visualizzo quello che è stato selzionato
         if ($(this).children(".dd-menu").is(":hidden")) {
 
             // chiudo tutti i dropdown menu eventualmente aperti
             $(".menu-item").children(".dd-menu").hide();
             // rimuovo l'evidenziazione "attiva" dalla voce del menu
+            // da tutti i menu, quindi anche uno eventualmente aperto
             $(".menu-item").removeClass("active-menu");
             // apro il dropdown menu della voce menu cliccata
             $(this).children(".dd-menu").toggle();
@@ -32,19 +33,14 @@ $(document).ready(function() {
         // }
     });
 
-    // #language-menu+.dd-menu span
     // intercetto click sulla dropdown menu per il cambio lingua
     $('#language-menu-dd span').click(function() {
 
-        // console.log("$(this).text", $(this).text());
-
         switch ($(this).text()) {
             case 'Italiano':
-                // console.log("it");
                 $('#language-menu').text("IT");
                 break;
             case 'English':
-                // console.log("eng");
                 $('#language-menu').text("EN");
                 break;
             default:
